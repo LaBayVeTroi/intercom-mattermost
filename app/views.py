bb08a25a85a576f3f05abb326a946dfb91c02e6a
LaBayVeTroi <namnguyenuetk62@gmail.com>
2020-03-11 16:29:28 +0700

    change config docker

--- app/views.py
+++ app/views.py
@@ -4,11 +4,14 @@ from django.core.cache import cache
 import redis
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 # Create your views here.
 
 TTL = 60 * 120
 MATTER_MOST_URL = 'https://mattermost-namnlnh.herokuapp.com/api/v4/'
+INTERCOM_APP_ID = 'j0pnttcr'
+INTERCOM_PING_URL = 'https://api-iam.intercom.io/messenger/web/ping'
 
 def hello_world(request):
   d = {
@@ -27,3 +30,16 @@ def recieve_intercom_hook(request):
   cache.set(user_id, user, timeout=TTL)
   return Response({'data': request.data})
 
+@api_view(['GET'])
+def ping_intercom():
+  req_data = {
+    'app_id' = INTERCOM_APP_ID
+  }
+  res = requests.post(url=INTERCOM_PING_URL,json=req_data)
+  res_json = res.json()
+  user_id = res_json.user.id
+  user
+
+
+@api_view(['GET'])
+def send_message_to_intercom():
